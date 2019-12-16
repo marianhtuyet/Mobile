5a0115789e3fb54d57b9c746f1fdbb2325be2d84
hathanhdoan <37020099+hathanhdoan@users.noreply.github.com>
2019-12-16 20:56:42 +0700

    update gio hang

--- FinalProject/app/src/main/java/com/example/finalproject/adapter/GioHangAdapter.java
+++ FinalProject/app/src/main/java/com/example/finalproject/adapter/GioHangAdapter.java
@@ -1,12 +1,14 @@
 package com.example.finalproject.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.finalproject.R;
@@ -48,7 +50,7 @@ public class GioHangAdapter extends BaseAdapter {
         public Button btnMinius, btnValue, btnPlus;
     }
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
         viewHolder =null;
         if (convertView == null){
             viewHolder = new ViewHolder();
@@ -82,27 +84,29 @@ public class GioHangAdapter extends BaseAdapter {
             viewHolder.btnPlus.setVisibility(View.VISIBLE);
             viewHolder.btnMinius.setVisibility(View.VISIBLE);
         }
+
         //Bắt sự kiện cho nút plus
         viewHolder.btnPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int slMoiNhat = Integer.parseInt(viewHolder.btnValue.getText().toString())+1;
+                Log.d("PLUS","plus"+position+"");
+                int slMoiNhat = Integer.parseInt(((Button)parent.getChildAt(position).findViewById(R.id.btnvalue)).getText().toString())+1;
                 int slHienTai = MainActivity.mangGioHang.get(position).getSoluong();
                 int giaHienTai = MainActivity.mangGioHang.get(position).getGiasp();
                 MainActivity.mangGioHang.get(position).setSoluong(slMoiNhat);
                 int giaMoiNhat = (giaHienTai * slMoiNhat)/slHienTai;
                 MainActivity.mangGioHang.get(position).setGiasp(giaMoiNhat);
                 DecimalFormat decimalFormat =  new DecimalFormat("###,###,###");
-                viewHolder.tvGiaGioHang.setText(decimalFormat.format(giaMoiNhat)+" Đ");
+                ((TextView)parent.getChildAt(position).findViewById(R.id.textview_GiaGioHang)).setText(decimalFormat.format(giaMoiNhat)+" Đ");
                 com.example.finalproject.activity.GioHang.totalValueGioHang();
                 if(slMoiNhat>=10){
                     viewHolder.btnPlus.setVisibility(View.INVISIBLE);
                     viewHolder.btnMinius.setVisibility(View.VISIBLE);
-                    viewHolder.btnValue.setText(String.valueOf(slMoiNhat));
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnvalue)).setText(String.valueOf(slMoiNhat));
                 }else {
-                    viewHolder.btnPlus.setVisibility(View.VISIBLE);
-                    viewHolder.btnMinius.setVisibility(View.VISIBLE);
-                    viewHolder.btnValue.setText(String.valueOf(slMoiNhat));
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnplus)).setVisibility(View.VISIBLE);
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnminus)).setVisibility(View.VISIBLE);
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnvalue)).setText(String.valueOf(slMoiNhat));
                 }
             }
         });
@@ -110,23 +114,23 @@ public class GioHangAdapter extends BaseAdapter {
         viewHolder.btnMinius.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int slMoiNhat = Integer.parseInt(viewHolder.btnValue.getText().toString())-1;
+                int slMoiNhat = Integer.parseInt(((Button)parent.getChildAt(position).findViewById(R.id.btnvalue)).getText().toString())-1;
                 int slHienTai = MainActivity.mangGioHang.get(position).getSoluong();
                 int giaHienTai = MainActivity.mangGioHang.get(position).getGiasp();
                 MainActivity.mangGioHang.get(position).setSoluong(slMoiNhat);
                 int giaMoiNhat = (giaHienTai * slMoiNhat)/slHienTai;
                 MainActivity.mangGioHang.get(position).setGiasp(giaMoiNhat);
                 DecimalFormat decimalFormat =  new DecimalFormat("###,###,###");
-                viewHolder.tvGiaGioHang.setText(decimalFormat.format(giaMoiNhat)+" Đ");
+                ((TextView)parent.getChildAt(position).findViewById(R.id.textview_GiaGioHang)).setText(decimalFormat.format(giaMoiNhat)+" Đ");
                 com.example.finalproject.activity.GioHang.totalValueGioHang();
                 if(slMoiNhat<=1){
-                    viewHolder.btnMinius.setVisibility(View.INVISIBLE);
-                    viewHolder.btnPlus.setVisibility(View.VISIBLE);
-                    viewHolder.btnValue.setText(String.valueOf(slMoiNhat));
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnminus)).setVisibility(View.INVISIBLE);
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnplus)).setVisibility(View.VISIBLE);
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnvalue)).setText(String.valueOf(slMoiNhat));
                 }else {
-                    viewHolder.btnPlus.setVisibility(View.VISIBLE);
-                    viewHolder.btnMinius.setVisibility(View.VISIBLE);
-                    viewHolder.btnValue.setText(String.valueOf(slMoiNhat));
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnplus)).setVisibility(View.VISIBLE);
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnminus)).setVisibility(View.VISIBLE);
+                    ((Button)parent.getChildAt(position).findViewById(R.id.btnvalue)).setText(String.valueOf(slMoiNhat));
                 }
             }
         });
